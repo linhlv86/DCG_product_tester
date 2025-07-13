976ec9c0e62cae899d80817a616171e130dc98bf
linhlv86 <6311397+linhlv86@users.noreply.github.com>
2025-07-13 15:11:29 +0700

    +

--- tasks/task_2_serial.py
+++ tasks/task_2_serial.py
@@ -3,6 +3,7 @@ import time
 import subprocess
 import json
 import re
+import glob
 
 # Mô tả của task này, sẽ hiển thị trên giao diện người dùng
 DESCRIPTION = "System information"
@@ -12,14 +13,18 @@ global_message =[]
 
 def test_task():
     detail_results = []
+    global_message = []
 
-   #1 Check for ls /dev/ttyUSB*
-   # it must be exist ttyUSB0..3
-    try:
-        output = subprocess.check_output(['/usr/bin/ls', '/dev/ttyUSB*'], text=True)
-        devices = output.strip().split('\n')
-        if not devices or len(devices) < 4:
-            raise FileNotFoundError("Not enough USB serial devices found.")
+    # Kiểm tra các thiết bị ttyUSB*
+    devices = glob.glob('/dev/ttyUSB*')
+    if not devices or len(devices) < 4:
+        detail_results.append({
+            "item": "USB serial devices",
+            "result": "FAIL",
+            "detail": "Not enough USB serial devices found.",
+            "passed": False
+        })
+    else:
         detail = "USB serial devices found: " + ", ".join(devices)
         global_message.append(detail)
         detail_results.append({
@@ -28,13 +33,7 @@ def test_task():
             "detail": detail,
             "passed": True
         })
-    except Exception as e:
-        detail_results.append({
-            "item": "USB serial devices",
-            "result": "FAIL",
-            "detail": str(e),
-            "passed": False
-        })
+
     # Tổng kết
     num_pass = sum(1 for r in detail_results if r["passed"])
     num_fail = len(detail_results) - num_pass
