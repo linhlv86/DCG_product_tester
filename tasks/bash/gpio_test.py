2c4da5d94860282eeb1c8f956033a0ce4aeb08e4
linhlv86 <6311397+linhlv86@users.noreply.github.com>
2025-07-15 18:39:02 +0700

    Update gpio_test.py

--- tasks/bash/gpio_test.py
+++ tasks/bash/gpio_test.py
@@ -1,6 +1,8 @@
 import subprocess
 import time
 
+GP_IOSET = "/usr/bin/gpioset"
+
 # Mapping các GPIO cần điều khiển
 gpio_119 = ("gpiochip3", "23")  # GPIO119
 other_gpios = [
@@ -17,12 +19,12 @@ def blink_gpios():
     while True:
         # Toggle GPIO119
         gpio119_state = 1 - gpio119_state
-        subprocess.run(["gpioset", gpio_119[0], f"{gpio_119[1]}={gpio119_state}"])
+        subprocess.run([GP_IOSET, gpio_119[0], f"{gpio_119[1]}={gpio119_state}"])
 
         # Chỉ bật 1 GPIO trong list, các GPIO còn lại tắt
         for i, gpio in enumerate(other_gpios):
             state = 1 if i == idx % len(other_gpios) else 0
-            subprocess.run(["gpioset", gpio[0], f"{gpio[1]}={state}"])
+            subprocess.run([GP_IOSET, gpio[0], f"{gpio[1]}={state}"])
 
         time.sleep(0.5)
         idx += 1
